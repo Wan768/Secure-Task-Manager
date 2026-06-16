57da18faffcf61eb9d9559b1d4edb28aa21bb736
Wan768 <wanahmadnaqeeb987@gmail.com>
2026-06-17 00:59:33 +0800

    Update admin.py

--- tasks/admin.py
+++ tasks/admin.py
@@ -4,16 +4,15 @@ from .models import Task, FailedLoginAttempt
 # Admin CRUD tasks for all user
 admin.site.register(Task)
 
-# creates a customized  grid for the security logs
+
 class FailedLoginAttemptAdmin(admin.ModelAdmin):
-    # The columns the admin will see
+    
     list_display = ('username', 'ip_address', 'timestamp')
-    # Adds a search bar to look up specific attempts
+    
     search_fields = ('username', 'ip_address')
-    # filter by date
+   
     list_filter = ('timestamp',)
     
-    # Make the logs read-only 
     def has_add_permission(self, request):
         return False
     def has_change_permission(self, request, obj=None):
